14ba7a58e4b3a8729d0b21dc712edb73c921681a
jessegood <gdjss2728@gmail.com>
2016-10-17 22:40:32 +0900

    Add project files.

---
+++ MXLIFFParser.cs
@@ -0,0 +1,432 @@
+﻿namespace Leo.FileTypeSupport.MXLIFF
+{
+    using Sdl.Core.Globalization;
+    using Sdl.Core.Settings;
+    using Sdl.FileTypeSupport.Framework.BilingualApi;
+    using Sdl.FileTypeSupport.Framework.Core.Utilities.NativeApi;
+    using Sdl.FileTypeSupport.Framework.IntegrationApi;
+    using Sdl.FileTypeSupport.Framework.NativeApi;
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Text.RegularExpressions;
+    using System.Xml;
+
+    internal class MXLIFFParser : AbstractBilingualFileTypeComponent, IBilingualParser, INativeContentCycleAware, ISettingsAware
+    {
+        private readonly Dictionary<int, string> users = new Dictionary<int, string>();
+        private XmlDocument document;
+        private IDocumentProperties documentProperties;
+        private IFileProperties fileProperties;
+        private XmlNamespaceManager nsmgr;
+        private int srcSegmentTagCount;
+        private int tmpTotalTagCount;
+        private int totalTagCount;
+        private int workflowLevel = 0;
+
+        public event EventHandler<ProgressEventArgs> Progress;
+
+        public IDocumentProperties DocumentProperties
+        {
+            get
+            {
+                return documentProperties;
+            }
+            set
+            {
+                documentProperties = value;
+            }
+        }
+
+        public IBilingualContentHandler Output
+        {
+            get;
+            set;
+        }
+
+        public void Dispose()
+        {
+            document = null;
+        }
+
+        public void EndOfInput()
+        {
+            OnProgress(100);
+            document = null;
+        }
+
+        public void InitializeSettings(ISettingsBundle settingsBundle, string configurationId)
+        {
+            // Loading of filter settings
+        }
+
+        public bool ParseNext()
+        {
+            if (documentProperties == null)
+            {
+                documentProperties = ItemFactory.CreateDocumentProperties();
+            }
+
+            Output.Initialize(documentProperties);
+
+            IFileProperties fileInfo = ItemFactory.CreateFileProperties();
+            fileInfo.FileConversionProperties = fileProperties.FileConversionProperties;
+            Output.SetFileProperties(fileInfo);
+
+            // Variables for the progress report
+            var xmlNodeList = document.SelectNodes("//x:group", nsmgr);
+            if (xmlNodeList != null)
+            {
+                int totalUnitCount = xmlNodeList.Count;
+                int currentUnitCount = 0;
+                foreach (XmlNode item in xmlNodeList)
+                {
+                    Output.ProcessParagraphUnit(CreateParagraphUnit(item));
+
+                    // Update the progress report
+                    currentUnitCount++;
+                    OnProgress(Convert.ToByte(Math.Round(100 * ((decimal)currentUnitCount / totalUnitCount), 0)));
+                }
+            }
+
+            Output.FileComplete();
+            Output.Complete();
+
+            return false;
+        }
+
+        public void SetFileProperties(IFileProperties properties)
+        {
+            fileProperties = properties;
+        }
+
+        public void StartOfInput()
+        {
+            OnProgress(0);
+            document = new XmlDocument();
+            document.Load(fileProperties.FileConversionProperties.OriginalFilePath);
+            nsmgr = new XmlNamespaceManager(document.NameTable);
+            nsmgr.AddNamespace("x", "urn:oasis:names:tc:xliff:document:1.2");
+            nsmgr.AddNamespace("m", "http://www.memsource.com/mxlf/2.0");
+
+            // Acquire workflow level
+            var level = document.DocumentElement.Attributes["m:level"];
+
+            if (level != null)
+            {
+                workflowLevel = Int32.Parse(level.Value);
+            }
+
+            // Acquire users
+            var memsourceUsers = document.SelectNodes("//m:user", nsmgr);
+
+            if (memsourceUsers != null)
+            {
+                foreach (XmlElement user in memsourceUsers)
+                {
+                    var id = user.Attributes["id"]?.Value;
+                    var username = user.Attributes["username"]?.Value;
+
+                    users.Add((id != null ? Convert.ToInt32(id) : 0), username);
+                }
+            }
+        }
+
+        protected virtual void OnProgress(byte percent)
+        {
+            Progress?.Invoke(this, new ProgressEventArgs(percent));
+        }
+
+        private ICommentProperties CreateComment(XmlNode commentNode)
+        {
+            ICommentProperties commentProperties = PropertiesFactory.CreateCommentProperties();
+
+            var createdBy = commentNode.Attributes["created-by"]?.Value;
+            var resolved = commentNode.Attributes["resolved"]?.Value;
+
+            string author = string.Empty;
+            if (createdBy != null)
+            {
+                author = users[Convert.ToInt32(createdBy)];
+            }
+
+            Severity severity = Severity.Low;
+
+            if (resolved != null && (resolved == "false"))
+            {
+                severity = Severity.Medium;
+            }
+
+            var comment = PropertiesFactory.CreateComment(commentNode.InnerText,
+                                                          author, severity);
+
+            commentProperties.Add(comment);
+
+            return commentProperties;
+        }
+
+        private ConfirmationLevel CreateConfirmationLevel(XmlNode transUnit)
+        {
+            ConfirmationLevel sdlxliffLevel;
+
+            if (transUnit != null)
+            {
+                var confirmed = transUnit.Attributes["m:confirmed"];
+                var levelEdited = transUnit.Attributes["m:level-edited"];
+
+                if (confirmed != null && (confirmed.Value == "1"))
+                {
+                    if (workflowLevel > 1 && levelEdited != null && (levelEdited.Value == "true"))
+                    {
+                        sdlxliffLevel = ConfirmationLevel.ApprovedTranslation;
+                    }
+                    else
+                    {
+                        sdlxliffLevel = ConfirmationLevel.Translated;
+                    }
+                }
+                else
+                {
+                    if (workflowLevel > 1 && levelEdited != null && (levelEdited.Value == "true"))
+                    {
+                        sdlxliffLevel = ConfirmationLevel.RejectedTranslation;
+                    }
+                    else
+                    {
+                        sdlxliffLevel = ConfirmationLevel.Draft;
+                    }
+                }
+            }
+            else
+            {
+                sdlxliffLevel = ConfirmationLevel.Unspecified;
+            }
+
+            return sdlxliffLevel;
+        }
+
+        private IContextProperties CreateContext(string id, string paraId)
+        {
+            IContextProperties contextProperties = PropertiesFactory.CreateContextProperties();
+
+            IContextInfo contextId = PropertiesFactory.CreateContextInfo("id");
+            contextId.SetMetaData("ID", id);
+            contextId.Description = "Trans-unit id";
+            contextId.DisplayCode = "ID";
+
+            IContextInfo contextParaId = PropertiesFactory.CreateContextInfo("para-id");
+            contextParaId.SetMetaData("PARA ID", paraId);
+            contextParaId.Description = "Para-id";
+            contextParaId.DisplayCode = "PARA ID";
+
+            contextProperties.Contexts.Add(contextId);
+            contextProperties.Contexts.Add(contextParaId);
+
+            return contextProperties;
+        }
+
+        private Byte CreateMatchValue(XmlNode transUnit)
+        {
+            Byte matchValue = 0;
+
+            // "m:gross-score" and "m:score" are both percentage values of TM matches from which the given segment is pre-translated.
+            // "m:gross-score" is the percentage prior to the application of TM penalization, if any, while "m:score" is the percentage after penalization.
+            var score = transUnit.Attributes["m:score"];
+
+            if (score != null)
+            {
+                matchValue = Convert.ToByte(Convert.ToDouble(score.Value) * 100);
+            }
+
+            return matchValue;
+        }
+
+        private IParagraphUnit CreateParagraphUnit(XmlNode xmlUnit)
+        {
+            // Create paragraph unit object
+            IParagraphUnit paragraphUnit = ItemFactory.CreateParagraphUnit(LockTypeFlags.Unlocked);
+
+            var xmlNode = xmlUnit.SelectSingleNode("x:trans-unit", nsmgr);
+
+            if (xmlNode != null)
+            {
+                var id = xmlNode.Attributes["id"];
+                var paraId = xmlNode.Attributes["m:para-id"];
+
+                if (id != null && paraId != null)
+                {
+                    paragraphUnit.Properties.Contexts = CreateContext(id.Value, paraId.Value);
+                }
+
+                // Create segment pair object
+                ISegmentPairProperties segmentPairProperties = ItemFactory.CreateSegmentPairProperties();
+                ITranslationOrigin tuOrg = ItemFactory.CreateTranslationOrigin();
+
+                // Assign the appropriate confirmation level to the segment pair
+                segmentPairProperties.ConfirmationLevel = CreateConfirmationLevel(xmlNode);
+                tuOrg.MatchPercent = CreateMatchValue(xmlNode);
+
+                // Add source segment to paragraph unit
+                ISegment srcSegment = CreateSegment(xmlNode.SelectSingleNode("x:source", nsmgr), segmentPairProperties, true);
+                paragraphUnit.Source.Add(srcSegment);
+
+                // Add target segment to paragraph unit if available
+                if (xmlNode.SelectSingleNode("x:target", nsmgr) != null)
+                {
+                    ISegment trgSegment = CreateSegment(xmlNode.SelectSingleNode("x:target", nsmgr), segmentPairProperties, false);
+
+                    // Check if locked
+                    var locked = xmlNode.Attributes["m:locked"];
+                    if (locked != null && locked.Value == "true")
+                    {
+                        trgSegment.Properties.IsLocked = true;
+                    }
+
+                    // Check if target empty and look for alt-trans
+                    if (trgSegment.Count == 0)
+                    {
+                        var alttrans = xmlNode.SelectSingleNode("x:alt-trans/x:target", nsmgr);
+                        if (alttrans != null && !string.IsNullOrWhiteSpace(alttrans.InnerText))
+                        {
+                            PopulateSegment(trgSegment, alttrans, false);
+
+                            var alttransOrigin = xmlNode.SelectSingleNode("x:alt-trans", nsmgr).Attributes["origin"];
+                            if (alttransOrigin != null)
+                            {
+                                var origin = alttransOrigin.Value;
+                                if (origin.Contains("machine") || origin.Contains("mt"))
+                                {
+                                    tuOrg.OriginType = DefaultTranslationOrigin.MachineTranslation;
+                                }
+                                else if (origin.Contains("tm"))
+                                {
+                                    tuOrg.OriginType = DefaultTranslationOrigin.TranslationMemory;
+                                }
+
+                                tuOrg.OriginSystem = origin;
+                            }
+                        }
+                    }
+
+                    paragraphUnit.Target.Add(trgSegment);
+                }
+                else
+                {
+                    var singleNode = xmlNode.SelectSingleNode("x:source", nsmgr);
+                    if (singleNode != null) singleNode.InnerText = "";
+                    ISegment trgSegment = CreateSegment(xmlNode.SelectSingleNode("x:source", nsmgr), segmentPairProperties, false);
+                    paragraphUnit.Target.Add(trgSegment);
+                }
+
+                var transOrigin = xmlNode.Attributes["m:trans-origin"];
+                if (transOrigin.Value != null && transOrigin.Value != "null")
+                {
+                    tuOrg.OriginType = transOrigin.Value;
+                }
+
+                segmentPairProperties.TranslationOrigin = tuOrg;
+
+                // Add comments
+                if (xmlNode.SelectSingleNode("m:comment", nsmgr) != null)
+                {
+                    paragraphUnit.Properties.Comments = CreateComment(xmlNode.SelectSingleNode("m:comment", nsmgr));
+                }
+            }
+
+            return paragraphUnit;
+        }
+
+        private IPlaceholderTag CreatePhTag(string tagContent, int tagNo, bool source)
+        {
+            IPlaceholderTagProperties phTagProperties = PropertiesFactory.CreatePlaceholderTagProperties(tagContent);
+            IPlaceholderTag phTag = ItemFactory.CreatePlaceholderTag(phTagProperties);
+
+            phTagProperties.TagContent = tagContent;
+            phTagProperties.DisplayText = string.Format("{{{0}}}", tagNo);
+            phTagProperties.CanHide = false;
+
+            if (source)
+            {
+                var thisId =
+                    new TagId(totalTagCount.ToString(CultureInfo.InvariantCulture));
+
+                phTagProperties.TagId = thisId;
+                totalTagCount += 1;
+                tmpTotalTagCount += 1;
+                srcSegmentTagCount += 1;
+            }
+            else
+            {
+                var thisId =
+                    new TagId(totalTagCount.ToString(CultureInfo.InvariantCulture));
+
+                phTagProperties.TagId = thisId;
+                totalTagCount += 1;
+            }
+
+            return phTag;
+        }
+
+        private ISegment CreateSegment(XmlNode segNode, ISegmentPairProperties pair, bool source)
+        {
+            ISegment segment = ItemFactory.CreateSegment(pair);
+
+            if (source)
+            {
+                srcSegmentTagCount = 0;
+                if (totalTagCount < tmpTotalTagCount)
+                {
+                    totalTagCount = tmpTotalTagCount;
+                }
+            }
+            else
+            {
+                totalTagCount = totalTagCount - srcSegmentTagCount;
+            }
+
+            PopulateSegment(segment, segNode, source);
+
+            return segment;
+        }
+
+        private IText CreateText(string segText)
+        {
+            ITextProperties textProperties = PropertiesFactory.CreateTextProperties(segText);
+            IText textContent = ItemFactory.CreateText(textProperties);
+
+            return textContent;
+        }
+
+        private void PopulateSegment(ISegment segment, XmlNode node, bool source)
+        {
+            int i = 1;
+            foreach (XmlNode item in node.ChildNodes)
+            {
+                if (item.NodeType == XmlNodeType.Text)
+                {
+                    foreach (var chunk in Regex.Split(item.InnerText, "((?:{|<)[a-z0-9]{1,2}(?:>|}))"))
+                    {
+                        if (Regex.IsMatch(chunk, "((?:{|<)[a-z0-9]{1,2}(?:>|}))"))
+                        {
+                            segment.Add(CreatePhTag(chunk, i, source));
+                            i++;
+                        }
+                        else
+                        {
+                            if (!string.IsNullOrEmpty(chunk))
+                            {
+                                segment.Add(CreateText(chunk));
+                            }
+                        }
+                    }
+                }
+
+                if (item.NodeType == XmlNodeType.Element)
+                {
+                    segment.Add(CreatePhTag(item.OuterXml, i, source));
+                    i++;
+                }
+            }
+        }
+    }
+}
---
+++ MXLIFFWriter.cs
@@ -0,0 +1,279 @@
+﻿using Sdl.Core.Globalization;
+using Sdl.FileTypeSupport.Framework.BilingualApi;
+using Sdl.FileTypeSupport.Framework.NativeApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace Leo.FileTypeSupport.MXLIFF
+{
+    internal class MXLIFFWriter : AbstractBilingualFileTypeComponent, IBilingualWriter, INativeOutputSettingsAware
+    {
+        private INativeOutputFileProperties nativeFileProperties;
+        private XmlNamespaceManager nsmgr;
+        private IPersistentFileConversionProperties originalFileProperties;
+        private XmlDocument targetFile;
+        private MXLIFFTextExtractor textExtractor;
+        private int workflowLevel = 0;
+        private Dictionary<string, int> users = new Dictionary<string, int>();
+
+        public void Complete()
+        {
+        }
+
+        public void Dispose()
+        {
+            // Don't need to dispose of anything
+        }
+
+        public void FileComplete()
+        {
+            using (XmlTextWriter wr = new XmlTextWriter(nativeFileProperties.OutputFilePath, Encoding.UTF8))
+            {
+                wr.Formatting = Formatting.None;
+                targetFile.Save(wr);
+                targetFile = null;
+            }
+        }
+
+        public void GetProposedOutputFileInfo(IPersistentFileConversionProperties fileProperties, IOutputFileInfo proposedFileInfo)
+        {
+            originalFileProperties = fileProperties;
+        }
+
+        public void Initialize(IDocumentProperties documentInfo)
+        {
+            textExtractor = new MXLIFFTextExtractor();
+        }
+
+        public void ProcessParagraphUnit(IParagraphUnit paragraphUnit)
+        {
+            string id = paragraphUnit.Properties.Contexts.Contexts[0].GetMetaData("ID");
+            XmlNode xmlUnit = targetFile.SelectSingleNode("//x:trans-unit[@id='" + id + "']", nsmgr);
+
+            CreateParagraphUnit(paragraphUnit, xmlUnit);
+        }
+
+        public void SetFileProperties(IFileProperties fileInfo)
+        {
+            targetFile = new XmlDocument();
+            targetFile.PreserveWhitespace = false;
+            targetFile.Load(originalFileProperties.OriginalFilePath);
+            nsmgr = new XmlNamespaceManager(targetFile.NameTable);
+            nsmgr.AddNamespace("x", "urn:oasis:names:tc:xliff:document:1.2");
+            nsmgr.AddNamespace("m", "http://www.memsource.com/mxlf/2.0");
+
+            var level = targetFile.DocumentElement.Attributes["m:level"];
+
+            if (level != null)
+            {
+                workflowLevel = Int32.Parse(level.Value);
+            }
+
+            // Acquire users
+            var memsourceUsers = targetFile.SelectNodes("//m:user", nsmgr);
+
+            if (memsourceUsers != null)
+            {
+                foreach (XmlElement user in memsourceUsers)
+                {
+                    var id = user.Attributes["id"]?.Value;
+                    var username = user.Attributes["username"]?.Value;
+
+                    users.Add(username, id != null ? Convert.ToInt32(id) : 0);
+                }
+            }
+        }
+
+        public void SetOutputProperties(INativeOutputFileProperties properties)
+        {
+            nativeFileProperties = properties;
+        }
+
+        private void AddComments(XmlNode xmlUnit, List<IComment> comments)
+        {
+            var text = string.Empty;
+            var comment = comments.First();
+
+            // We concatenate all comment text if there are multiple
+            foreach (var c in comments)
+            {
+                text += c.Text + " ";
+            }
+
+            var createdat = targetFile.CreateAttribute("created-at");
+            var createdby = targetFile.CreateAttribute("created-by");
+            var modifiedat = targetFile.CreateAttribute("modified-at");
+            modifiedat.Value = "0";
+
+            var modifiedby = targetFile.CreateAttribute("modified-by");
+
+            var resolved = targetFile.CreateAttribute("resolved");
+            resolved.Value = "false";
+
+            string commentDate = this.GetCommentDate();
+
+            // Convert DateTime to Unix timestamp in milliseconds
+            long milliseconds = (long)(TimeZoneInfo.ConvertTimeToUtc(comment.Date) - new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds;
+            createdat.Value = milliseconds.ToString();
+
+            // Try to find the user id by author name
+            // If it fails, just leave it blank
+            if (users.ContainsKey(comment.Author))
+            {
+                createdby.Value = users[comment.Author].ToString();
+            }
+            else
+            {
+                createdby.Value = "";
+            }
+
+            xmlUnit.Attributes.Append(createdat);
+            xmlUnit.Attributes.Append(createdby);
+            xmlUnit.Attributes.Append(modifiedat);
+            xmlUnit.Attributes.Append(modifiedby);
+            xmlUnit.Attributes.Append(resolved);
+
+            xmlUnit.InnerText = text;
+        }
+
+        private void CreateParagraphUnit(IParagraphUnit paragraphUnit, XmlNode transUnit)
+        {
+            // Iterate all segment pairs
+            foreach (ISegmentPair segmentPair in paragraphUnit.SegmentPairs)
+            {
+                Byte matchPercent = 0;
+
+                if (transUnit != null)
+                {
+                    XmlNode source = transUnit.SelectSingleNode("x:source", nsmgr);
+
+                    if (source != null)
+                    {
+                        source.InnerText = textExtractor.GetPlainText(segmentPair.Source);
+                    }
+
+                    if (segmentPair.Properties.TranslationOrigin != null)
+                    {
+                        matchPercent = segmentPair.Properties.TranslationOrigin.MatchPercent;
+                    }
+                }
+
+                XmlNode target;
+                if (transUnit.SelectSingleNode("x:target", nsmgr) == null)
+                {
+                    XmlDocument segDoc = new XmlDocument();
+                    string nodeContent = transUnit.OuterXml;
+                    segDoc.LoadXml(nodeContent);
+                    XmlNode trgNode = segDoc.CreateNode(XmlNodeType.Element, "target", nsmgr.DefaultNamespace);
+
+                    XmlNode importNode = transUnit.OwnerDocument.ImportNode(trgNode, true);
+                    transUnit.AppendChild(importNode);
+                    transUnit.SelectSingleNode("x:target", nsmgr).InnerXml =
+                        textExtractor.GetPlainText(segmentPair.Target);
+                }
+                else
+                {
+                    target = transUnit.SelectSingleNode("x:target", nsmgr);
+                    target.InnerText = textExtractor.GetPlainText(segmentPair.Target);
+                }
+
+                // Add comments (if applicable)
+                var comments = textExtractor.GetSegmentComment(segmentPair.Target);
+                if (comments.Count > 0 && transUnit.SelectSingleNode("m:comment", nsmgr) == null)
+                {
+                    XmlElement commentElement = targetFile.CreateElement("m:comment", nsmgr.LookupNamespace("m"));
+
+                    var tunitMetaData = transUnit.SelectSingleNode("m:tunit-metadata", nsmgr);
+                    if (tunitMetaData != null)
+                    {
+                        transUnit.InsertBefore(commentElement, tunitMetaData);
+                        AddComments(transUnit.SelectSingleNode("m:comment", nsmgr), comments);
+                    }
+                }
+
+                // Update score value
+                var dbl = matchPercent / 100.0;
+                if (transUnit.Attributes["m:score"] != null)
+                {
+                    transUnit.Attributes["m:score"].Value = dbl.ToString();
+                }
+                else
+                {
+                    transUnit.Attributes.Append(transUnit.OwnerDocument.CreateAttribute("m:score"));
+                    transUnit.Attributes["m:score"].Value = dbl.ToString();
+                }
+
+                // Update m:locked
+                if (transUnit.Attributes["m:locked"] != null)
+                {
+                    var isLocked = segmentPair.Target?.Properties?.IsLocked.ToString();
+                    transUnit.Attributes["m:locked"].Value = isLocked != null ? isLocked.ToLower() : "false";
+                }
+
+                // Update m:confirmed
+                if (transUnit.Attributes["m:confirmed"] != null)
+                {
+                    if (segmentPair.Target != null && segmentPair.Target.Properties != null)
+                    {
+                        switch (segmentPair.Target.Properties.ConfirmationLevel)
+                        {
+                            case ConfirmationLevel.Unspecified:
+                                transUnit.Attributes["m:confirmed"].Value = "0";
+                                break;
+
+                            case ConfirmationLevel.Draft:
+                                transUnit.Attributes["m:confirmed"].Value = "0";
+                                break;
+
+                            case ConfirmationLevel.Translated:
+                                transUnit.Attributes["m:confirmed"].Value = "1";
+                                break;
+
+                            case ConfirmationLevel.RejectedTranslation:
+                                transUnit.Attributes["m:confirmed"].Value = "0";
+                                break;
+
+                            case ConfirmationLevel.ApprovedTranslation:
+                                transUnit.Attributes["m:confirmed"].Value = "1";
+                                break;
+
+                            case ConfirmationLevel.RejectedSignOff:
+                                transUnit.Attributes["m:confirmed"].Value = "0";
+                                break;
+
+                            case ConfirmationLevel.ApprovedSignOff:
+                                transUnit.Attributes["m:confirmed"].Value = "0";
+                                break;
+
+                            default:
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+        private string GetCommentDate()
+        {
+            string day;
+            string month;
+
+            if (DateTime.UtcNow.Month.ToString().Length == 1)
+                month = "0" + DateTime.UtcNow.Month;
+            else
+                month = "0" + DateTime.UtcNow.Month;
+
+            if (DateTime.UtcNow.Day.ToString().Length == 1)
+                day = "0" + DateTime.UtcNow.Day;
+            else
+                day = "0" + DateTime.UtcNow.Day;
+
+            return DateTime.UtcNow.Year + month + day + "T" +
+                DateTime.UtcNow.Hour + DateTime.UtcNow.Minute +
+                DateTime.UtcNow.Second + "Z";
+        }
+    }
+}
